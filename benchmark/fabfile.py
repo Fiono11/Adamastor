c507d9c91da769714c69b1fa34f1b759c70215b0
Fiono11 <ruipedromorais11@gmail.com>
2023-06-28 14:22:47 +0100

    wip

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -106,7 +106,7 @@ def remote(ctx, debug=False):
         'nodes': [7],
         'workers': 1,
         'collocate': True,
-        'rate': [10000],
+        'rate': [5000],
         'tx_size': 532,
         'duration': 60,
         'runs': 1,
